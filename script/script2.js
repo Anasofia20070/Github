77535381c3d4ed3182dbedefe4d7b903a990d0ed
Anasofia20070 <anapulidobedoya@iesantaelena.edu.co>
2023-03-29 16:12:07 -0500

    modificacion

--- script/script2.js
+++ script/script2.js
@@ -1,57 +1,55 @@
-let Propietario, Modelo, Placa, tipo, fecha, Inicio,  Horas;
-let formulario=document.getElementById("form2")
+let propietario, modelo, placa, tipo, fecha, hora, cantidad;
+
+let formulario = document.getElementById('form')
 
 formulario.addEventListener('submit', (e)=>{
     e.preventDefault()
     LeerDatos()
 })
 
-function LeerDatos(Propietario,Modelo,Placa,tipo,fecha,Inicio,Horas) {
-    Propietario = document.getElementById('Propietario').value
-    Modelo = document.getElementById('Modelo').value
-    Placa = document.getElementById('Placa').value
-    tipo = document.getElementById('tipo').value
-    fecha = document.getElementById('fecha').value
-    Inicio = document.getElementById('Inicio').value
-    Horas = document.getElementById('Horas').value
-    ValidarData(Propietario,Modelo,Placa,tipo,fecha,Horas)
-    GuardarLocalStorage(Propietario,Modelo,Placa,tipo,fecha,Inicio,Horas)
+function LeerDatos(propietario, modelo, placa, tipo, fecha, hora, cantidad){
+    propietario =document.getElementById('propietario').value
+    modelo =document.getElementById('modelo').value
+    placa =document.getElementById('placa').value
+    tipo =document.getElementById('tipo').value
+    feche =document.getElementById('fecha').value
+    hora =document.getElementById('hora').value
+    cantidad =document.getElementById('cantidad').value
+    
+    GuardarLocalStorage(propietario, modelo, placa, tipo, fecha, hora, cantidad)
 }
 
-function ValidarData(Propietario,Modelo,Placa,tipo,fecha,Inicio,Horas) {
-    alert()
-    if(Propietario.length==0 || Modelo.length==0  || Placa.length==0 || tipo.length==0 || fecha.length==0 || Inicio.length==0 || Horas.length==0){
-        swal.fire({
-            title:'Error!',
-            text:'Do you want to continue?',
-            icon:'error',
-            confirmButtonText: 'cool',
-            iconColor: 'purple'
-
+function validardata(propietario, modelo, placa, tipo, fecha, hora, cantidad){
+    if(propietario.length == 0 || modelo.length == 0 || placa.length == 0 || tipo.length == 0 || fecha.length == 0 || hora.length == 0 || cantidad.length == 0) {
+        Swal.fire({
+            title: 'Error!',
+            text: 'Do you want to continue',
+            icon: 'error',
+            confirmButtonText: 'Cool',
+            iconcolor: 'pink',
         })
     }
+    GuardarLocalStorage(propietario, modelo, placa, tipo, fecha, hora, cantidad)
 }
 
-function GuardarLocalStorage(Propietario,Modelo,Placa,tipo,fecha,Inicio,Horas) {
-    localStorage.setItem('Propietario del vehiculo',Propietario)
-    localStorage.setItem('Modelo del vehiculo',Modelo)
-    localStorage.setItem('Placa',Placa)
-    localStorage.setItem('tipo',tipo)
-    localStorage.setItem('fecha de Entrada',fecha)
-    localStorage.setItem('Hora de inicio',Inicio)
-    localStorage.setItem('Cantidad de horas',Horas)
-    ListarData()
-
+function GuardarLocalStorage(propietario, modelo, placa, tipo, fecha, hora, cantidad){
+    localStorage.setItem('propietario', propietario)
+    localStorage.setItem('modelo',modelo )
+    localStorage.setItem('placa',placa )
+    localStorage.setItem('tipo',tipo )
+    localStorage.setItem('fecha',fecha )
+    localStorage.setItem('hora',hora )
+    localStorage.setItem('cantidad',cantidad )
+    listarData()
 }
 
-function ListarData() {
-    let PropietarioUsu = localStorage.getItem('Propietario del vehiculo')
-    let ModeloUsu = localStorage.getItem('Modelo del vehiculo')
-    let PlacaUsu = localStorage.getItem('Placa')
+function listarData(propietario, modelo, placa, tipo, fecha, hora, cantidad) {
+    let propietarioUsu = localStorage.getItem('propietario')
+    let modeloUsu = localStorage.getItem('modelo')
+    let placaUsu = localStorage.getItem('placa')
     let tipoUsu = localStorage.getItem('tipo')
-    let fechaUsu = localStorage.getItem('fecha de Entrada')
-    let InicioUsu = localStorage.getItem('Hora de inicio')
-    let HorasUsu = localStorage.getItem('Cantidad de horas')
-
-    
+    let fechaUsu = localStorage.getItem('fecha')
+    let horaUsu = localStorage.getItem('hora')
+    let cantidadUsu = localStorage.getItem('cantidad')
 }
+
